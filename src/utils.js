700aebc00600387df09cb7ba0b3e3cc43905389c
dongyuanxin <yuanxin.me@gmail.com>
2019-05-09 20:06:41 +0800

    export PowerDate function

---
+++ src/utils.js
@@ -0,0 +1,26 @@
+function addZeroStr(num) {
+  if (num < 10) {
+    return '0' + num
+  } else {
+    return num + ''
+  }
+}
+
+export function PowerDate() {
+  const date = new Date()
+  
+  date.now = date.getTime()
+
+  date.format = () => {
+    const year = addZeroStr(date.getFullYear()),
+      month = addZeroStr(date.getMonth() + 1),
+      day = addZeroStr(date.getDate()),
+      hour = addZeroStr(date.getHours()),
+      minute = addZeroStr(date.getMinutes()),
+      second = addZeroStr(date.getSeconds())
+
+    return `${year}/${month}/${day} ${hour}:${minute}:${second}`
+  }
+
+  return date
+}
