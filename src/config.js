b0f8433d1dce4caadac0766050813807a1980071
dongyuanxin <yuanxin.me@gmail.com>
2019-05-10 13:09:04 +0800

    add config.js read window config auto

--- src/config.js
+++ src/config.js
@@ -1,3 +1,24 @@
-const config = window.VVIEW_CONFIG || {}
+const config = window.PAGE_COUNTER_CONFIG 
+const AV = window.AV 
 
-export default config
+const required = {
+  serverless: '',
+  leancloud: {
+    table: 'string',
+    appId: 'string',
+    appKey: 'string'
+  }
+}
+
+if (!config) {
+  throw new Error('Please init variable window.PAGE_COUNTER_CONFIG')
+}
+
+if (!AV) {
+  throw new Error('Please import leancloud-storage sdk')
+}
+
+export default {
+  AV,
+  ...config
+}
